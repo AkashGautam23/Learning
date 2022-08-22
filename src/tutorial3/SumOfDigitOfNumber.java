a708e6b51c318ceaf8936bd33bb98ec463e77f30
Akashkumar23 <akashgautamkumar@outlook.com>
2022-08-22 20:21:21 +0530

    Initial Completed

---
+++ src/tutorial3/SumOfDigitOfNumber.java
@@ -0,0 +1,18 @@
+package tutorial3;
+
+import java.util.Scanner;
+
+public class SumOfDigitOfNumber {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter number");
+        int num=sc.nextInt();
+        int sum=0,temp;
+        while(num!=0){
+            temp=num%10;
+            sum+=temp;
+            num/=10;
+        }
+        System.out.println("Sum of Digit of number "+sum);
+    }
+}
