0223e636a045fc423d92592084f3224e2a121d0d
Akashkumar23 <akashgautamkumar@outlook.com>
2022-08-25 10:27:09 +0530

    Initial version

---
+++ src/tutorial4/Factorial.java
@@ -0,0 +1,34 @@
+package tutorial4;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Number :");
+        int num= sc.nextInt();
+        System.out.println("Factorial :"+CheckFactorial(num));
+        System.out.println("Factorial :"+NormalFactorial(num));
+    }
+
+     static long CheckFactorial(int num) {
+        if(num==1)
+            return 1;
+        else {
+            return num * (CheckFactorial(num - 1));
+        }
+    }
+    static long NormalFactorial(int num){
+        long fact;
+        if(num==1)
+            return 1;
+        else{
+            fact=1;
+            while(num!=1){
+                fact*=num;
+                num--;
+            }
+        }
+        return fact;
+    }
+}
